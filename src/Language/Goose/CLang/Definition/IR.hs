f8ac2490f01fec389c85b86d255e54f7f1cd4639
thomasvergne <thomas.vergne.contact@gmail.com>
2023-04-04 15:22:04 +0200

    🏷️ Added CLang IR definition.

---
+++ src/Language/Goose/CLang/Definition/IR.hs
@@ -0,0 +1,46 @@
+module Language.Goose.CLang.Definition.IR where
+import Language.Goose.CST.Literal
+
+data IRToplevel
+  = IRFunction String [String] [IRStatement]
+  | IRDeclaration String IRExpression
+  | IRExtern String CType
+  | IRDeclare String [CType] CType
+  | IRStruct String [IRStructField]
+  deriving Eq
+
+data IRStructField 
+  = IRStructField String CType
+  | IRStructUnion String [IRStructField]
+  | IRStructStruct String [IRStructField]
+  deriving Eq
+
+data IRStatement 
+  = IRReturn IRExpression
+  | IRIf IRExpression [IRStatement] [IRStatement]
+  | IRWhile IRExpression [IRStatement]
+  | IRFor String IRExpression [IRStatement]
+  | IRExpression IRExpression
+  | IRBlock [IRStatement]
+  | IRBreak
+  | IRContinue
+  | IRDeclarationStatement String IRExpression
+  | IRUpdate IRExpression IRExpression
+  deriving Eq
+
+data IRExpression
+  = IRVariable String 
+  | IRApplication IRExpression [IRExpression]
+  | IRBinary String IRExpression IRExpression
+  | IRUnary String IRExpression
+  | IRLiteral Literal
+  | IRList [IRExpression]
+  | IRListAccess IRExpression IRExpression
+  | IRTernary IRExpression IRExpression IRExpression
+  | IREUpdate IRExpression IRExpression
+  | IRDict [(String, IRExpression)]
+  | IRReference IRExpression
+  | IRDictAccess IRExpression String
+  deriving Eq
+
+type CType = String
