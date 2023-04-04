a91385f87fd56c88c125357c163eea4c30823964
thomasvergne <thomas.vergne.contact@gmail.com>
2023-04-04 15:20:59 +0200

    🏷️ Added ANF monad module.

---
+++ src/Language/Goose/Transformation/ANF/Monad.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TupleSections #-}
+module Language.Goose.Transformation.ANF.Monad where
+import Control.Monad.RWS
+import Language.Goose.Transformation.ANF.AST
+
+type MonadANF m = (MonadRWS [(String, String)] () Int m)
+
+fresh :: MonadANF m => m String
+fresh = do
+  i <- get
+  put (i + 1)
+  return $ "$a" ++ show i
+
+createLet :: [(String, ANFExpression)] -> [ANFStatement]
+createLet [] = []
+createLet ((name', e) : xs) = SLet name' e : createLet xs
+
+createLetTL :: [(String, ANFExpression)] -> [ANFDefinition]
+createLetTL [] = []
+createLetTL ((name', e) : xs) = DDeclaration name' e : createLetTL xs
