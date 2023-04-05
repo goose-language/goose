482ccb5db9a3b6fa05f9f659f8a612fd5f431e17
thomasvergne <thomas.vergne.contact@gmail.com>
2023-04-05 15:03:26 +0200

    💄 Added ANF custom show instance.

--- src/Language/Goose/Transformation/ANF/AST.hs
+++ src/Language/Goose/Transformation/ANF/AST.hs
@@ -2,12 +2,18 @@ module Language.Goose.Transformation.ANF.AST where
 import Language.Goose.CST.Literal ( Literal )
 import Language.Goose.CST.Annoted ( Annoted )
 import Language.Goose.Typecheck.Definition.Type
+import Data.List
 
 data ANFUpdated 
   = UVariable String
   | UListAccess ANFUpdated ANFExpression
   | UStructAccess ANFUpdated String
-  deriving (Eq, Show)
+  deriving Eq
+
+instance Show ANFUpdated where
+  show (UVariable name) = name
+  show (UListAccess updated expr) = show updated ++ "[" ++ show expr ++ "]"
+  show (UStructAccess updated name) = show updated ++ "." ++ name
 
 data ANFExpression 
   = EVariable String
@@ -22,7 +28,21 @@ data ANFExpression
   | EUpdate ANFUpdated ANFExpression
   | EStructure [(String, ANFExpression)]
   | EStructAccess ANFExpression String
-  deriving (Eq, Show)
+  deriving Eq
+
+instance Show ANFExpression where
+  show (EVariable name) = name
+  show (ELiteral literal) = show literal
+  show (EApplication expr exprs) = show expr ++ "(" ++ intercalate ", " (map show exprs) ++ ")"
+  show (EBinary op expr1 expr2) = show expr1 ++ " " ++ op ++ " " ++ show expr2
+  show (EUnary op expr) = op ++ show expr
+  show (EIf cond then' else') = "if " ++ show cond ++ " then " ++ show then' ++ " else " ++ show else'
+  show (ELambda args body) = "lambda " ++ show args ++ " -> " ++ show body
+  show (EList exprs) = "[" ++ show exprs ++ "]"
+  show (EListAccess list index) = show list ++ "[" ++ show index ++ "]"
+  show (EUpdate updated expr) = show updated ++ " = " ++ show expr
+  show (EStructure fields) = "{" ++ intercalate ", " (map (\(n, e) -> n ++ ": " ++ show e) fields) ++ "}"
+  show (EStructAccess struct field) = show struct ++ "." ++ field
 
 data ANFStatement
   = SLet String ANFExpression
@@ -35,11 +55,29 @@ data ANFStatement
   | SBreak
   | SUpdate ANFUpdated ANFExpression
   | SContinue
-  deriving (Eq, Show)
+  deriving Eq
+
+instance Show ANFStatement where
+  show (SLet name expr) = "let " ++ name ++ " = " ++ show expr
+  show (SReturn expr) = "return " ++ show expr
+  show (SIf cond then' else') = "if " ++ show cond ++ " then " ++ show then' ++ " else " ++ show else'
+  show (SWhile cond body) = "while " ++ show cond ++ " do " ++ show body
+  show (SFor name list body) = "for " ++ name ++ " in " ++ show list ++ " do " ++ show body
+  show (SExpression expr) = show expr
+  show (SBlock body) = "block " ++ show body
+  show (SBreak) = "break"
+  show (SUpdate updated expr) = show updated ++ " = " ++ show expr
+  show (SContinue) = "continue"
 
 data ANFDefinition
   = DFunction String [String] [ANFStatement]
   | DDeclaration (String) ANFExpression
   | DExtern (Annoted Type)
   | DDeclare (Annoted Type)
-  deriving (Eq, Show)
+  deriving Eq
+
+instance Show ANFDefinition where
+  show (DFunction name args body) = "function " ++ name ++ "(" ++ show args ++ ") -> " ++ show body
+  show (DDeclaration name expr) = "declaration " ++ name ++ " = " ++ show expr
+  show (DExtern type') = "extern " ++ show type'
+  show (DDeclare type') = "declare " ++ show type'
