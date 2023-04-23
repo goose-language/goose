39ff9eba14976c3b30bea20d390ff9878a459608
thomasvergne <thomas.vergne.contact@gmail.com>
2023-04-24 00:03:09 +0200

    🍱 Added CLI runner on main script.

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,5 @@
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
-{-# LANGUAGE LambdaCase #-}
 module Main where
-import Language.Goose.Parser.Parser
-import Language.Goose.Module.Resolver
-import Language.Goose.Module.Bundler
-import Language.Goose.Typecheck.Checker
-import Language.Goose.Transformation.ANF.ANF
-import Language.Goose.Transformation.Closure.Conversion
-import Language.Goose.Transformation.EtaExpansion
-import Language.Goose.Transformation.DeclarationRemover
-import Language.Goose.LLVM.BuildLibrary (buildExecutable)
-import System.Environment
-import Data.List
-
-import qualified Log.Error as L
-
-
-chunkBy2 :: [a] -> [(a, a)]
-chunkBy2 [] = []
-chunkBy2 (x:y:xs) = (x, y) : chunkBy2 xs
-chunkBy2 _ = error "chunkBy2: odd number of elements"
+import CLI.CLI (runCLI)
 
 main :: IO ()
-main = do
-  getArgs >>= \case
-    file:libraries -> do
-      content <- readFile file
-      ast <- parseGoose file content
-      case ast of
-        Left err -> L.printParseError err file content
-        Right ast' -> do
-          ast <- runModuleResolver file ast'
-          case ast of
-            Left err -> L.printError (fst err, Nothing, snd err) "Module resolution"
-            Right ast' -> do
-              ast <- runModuleBundling $ nub ast'
-              case ast of
-                Left err -> L.printError (fst err, Nothing, snd err) "Module bundling"
-                Right ast' -> do
-                  ast <- performInfer $ nub ast'
-                  case ast of
-                    Left err -> L.printError err "Type inference"
-                    Right ast' -> do
-                      ast <- return $ runEtaExpansion ast'
-                      ast <- runANF ast
-                      ast <- return $ runClosureConversion ast
-                      ast <- return $ addInitFunction ast
-                      buildExecutable ast libraries
-    _ -> putStrLn "Usage: goose <file> [libraries]"
+main = runCLI
