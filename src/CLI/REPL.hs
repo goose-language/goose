cd7c1385833345a22273e2e6ac1ec49deb8fff66
thomasvergne <thomas.vergne.contact@gmail.com>
2023-04-24 21:50:06 +0200

    ⚡️ Updated REPL with library.

--- src/CLI/REPL.hs
+++ src/CLI/REPL.hs
@@ -1,48 +1,110 @@
+{-# LANGUAGE FlexibleContexts #-}
 module CLI.REPL where
-
-import Language.Goose.Parser.Parser
+import Language.Goose.Parser.Parser ( parseExpression, runGoose )
 import Language.Goose.CST.Expression (Toplevel (Function), Expression (Sequence, Let))
 import Language.Goose.CST.Located (Located)
 import Language.Goose.Parser.Modules.Toplevel (parseToplevel)
-import Language.Goose.Parser.Lexer (parseEither)
+import Language.Goose.Parser.Lexer (parseEither, reservedWords)
 import qualified Text.Parsec as P
 import Text.Parsec (ParseError)
-import System.IO (hFlush, stdout, hSetBuffering, BufferMode (NoBuffering))
 import CLI.Phases (getANFDefinitions)
 import qualified Language.Goose.CST.Located as C
 import qualified Language.Goose.CST.Annoted as C
 import Language.Goose.LLVM.BuildLibrary (buildAndRun)
-import Control.Monad (forM_)
 import qualified Log.Error as L
+import System.Console.Repline
+import Control.Monad.State
+import qualified Control.Monad.RWS as ST
+import Data.List (isPrefixOf)
 
 runParserREPL :: Monad m => String -> m (Either ParseError (Either (Located Expression) (Located Toplevel)))
 runParserREPL input = runGoose "REPL" input $ parseEither (parseExpression <* P.eof) (parseToplevel parseExpression)
 
-runREPL :: [Located Toplevel] -> [String] -> IO ()
-runREPL ast libraries = do
-  hSetBuffering stdout NoBuffering
-  putStr "goose> "
-  input <- getLine
-  case input of
-    ":q" -> return ()
-    [] -> runREPL ast libraries
-    _ -> do
-      result <- runParserREPL input
-      case result of
-        Left err -> L.printParseError err "REPL" input >> runREPL ast libraries
-        Right (Right toplevel) -> runREPL (ast ++ [toplevel]) libraries
-        Right (Left expression) -> case expression of
-          z@(Let {} C.:>: _) -> runREPL (insertInMain z ast) libraries
-          _ -> do
-            let ast' = insertInMain expression ast
-            anf <- getANFDefinitions input ast' "REPL"
-            case anf of
-              Nothing -> runREPL ast libraries
-              Just anf -> do
-                mapM_ print anf
-                res <- buildAndRun anf libraries []
-                forM_ res putStrLn
-                runREPL ast libraries
+data REPLState = REPLState {
+  ast :: [Located Toplevel],
+  libraries :: [String],
+  flags :: [String],
+  autocompletes :: [String]
+} deriving Show
+
+type REPL a = HaskelineT (StateT REPLState IO) a
+
+eval :: String -> REPL ()
+eval content = do
+  libraries' <- gets libraries
+  flags' <- gets flags
+  result <- liftIO $ runParserREPL content
+  case result of
+    Left err -> liftIO $ L.printParseError err "REPL" content
+    Right (Right toplevel) -> do
+      modify (\s -> s { ast = ast s ++ [toplevel] })
+    Right (Left expression) -> case expression of
+      z@(Let (C.Annoted name _) _ _ C.:>: _) -> ST.modify $ \s -> s { 
+        ast = insertInMain z (ast s),
+        autocompletes = name : autocompletes s }
+      _ -> do
+        ast' <- gets ast
+        let ast'' = insertInMain expression ast'
+        anf <- liftIO $ getANFDefinitions content ast'' "REPL"
+        case anf of
+          Nothing -> return ()
+          Just anf' -> do
+            res <- liftIO $ buildAndRun anf' libraries' flags'
+            liftIO $ forM_ res putStrLn
+
+completion :: (Monad m, MonadState REPLState m) => WordCompleter m
+completion n = do
+  autocompletes' <- gets autocompletes
+  return $ filter (isPrefixOf n) autocompletes'
+
+help :: String -> REPL ()
+help _ = liftIO $ putStrLn "Commands: :help, :quit, :ast, :libraries, :flags, :addLibrary, :addFlag"
+
+addLibrary :: String -> REPL ()
+addLibrary libraries' = do
+  let libraries'' = words libraries'
+  modify (\s -> s { libraries = libraries'' ++ libraries s })
+  liftIO $ putStrLn $ "Added library " ++ unwords libraries''
+
+addFlag :: String -> REPL ()
+addFlag flags' = do
+  let flags'' = words flags'
+  modify (\s -> s { flags = flags'' ++ flags s })
+  liftIO $ putStrLn $ "Added flag " ++ unwords flags''
+
+showLibraries :: REPL ()
+showLibraries = do
+  libraries' <- gets libraries
+  if null libraries'
+    then liftIO $ putStrLn "No libraries"
+    else liftIO $ putStrLn $ "Libraries: " ++ unwords libraries'
+
+showFlags :: REPL ()
+showFlags = do
+  flags' <- gets flags
+  if null flags'
+    then liftIO $ putStrLn "No flags"
+    else liftIO $ putStrLn $ "Flags: " ++ unwords flags'
+
+options' :: [(String, String -> REPL ())]
+options' = [("help", help), ("quit", const abort), ("libraries", const showLibraries), ("flags", const showFlags), ("addLibrary", addLibrary), ("addFlag", addFlag)]
+
+final :: REPL ExitDecision
+final = return Exit
+
+init' :: REPL ()
+init' = do
+  liftIO $ putStrLn "Welcome to Goose REPL"
+  liftIO $ putStrLn "Type :help for help"
+
+inputMode :: MultiLine -> REPL String
+inputMode SingleLine = return "> "
+inputMode MultiLine = return "| "
+
+runREPL :: IO ()
+runREPL = do
+  let initialState = REPLState [] [] [] reservedWords
+  flip evalStateT initialState $ evalRepl inputMode eval options' (Just ':') (Just "multiline") (Word completion) init' final 
 
 insertInMain :: Located Expression -> [Located Toplevel] -> [Located Toplevel]
 insertInMain expression (Function (C.Annoted "main" ty) gens args body C.:>: pos : xs) = do
